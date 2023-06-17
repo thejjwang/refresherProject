94d794e34d2f83b2ec0a3049249be7f5ebb81ec7
thejjwang <jonathanjacobwang@gmail.com>
2023-06-16 23:50:28 -0700

    added loginForm

--- index.js
+++ index.js
@@ -5,3 +5,22 @@ btn.addEventListener('click', changeContent);
 function changeContent() {
     content.innerText = "Hello World!";
 }
+
+let loginForm = document.getElementById("loginForm");
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+  
+    if (name.value == "" || email.value == "") {
+      alert("Ensure you input a value in both fields!");
+    } else {
+      alert("This form has been successfully submitted!");
+      console.log(
+        `This form has a username of ${name.value} and email of ${email.value}`
+      );
+      name.value = "";
+      email.value = "";
+    }
+  });
